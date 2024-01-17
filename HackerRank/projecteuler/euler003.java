27691aab1e0788b4890c7385c81409c42e2fc0af
Abhirama Sonny <abhirama.sonny@gmail.com>
2024-01-16 22:47:55 -0600

    added

---
+++ HackerRank/projecteuler/euler003.java
@@ -0,0 +1,41 @@
+package HackerRank.projecteuler;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+//What is the largest prime factor of a given number N?
+public class euler003 {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            long n = in.nextLong();
+            long largestPrimeFactor = getLargestPrimeFactor(n);
+            System.out.println(largestPrimeFactor);
+        }
+    }
+
+    private static long getLargestPrimeFactor(long n) {
+        long largestPrime = -1;
+
+        while (n % 2 == 0) {
+            largestPrime = 2;
+            n /= 2;
+        }
+
+        for (long i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                largestPrime = i;
+                n /= i;
+            }
+        }
+
+        if (n > 2) {
+            largestPrime = n;
+        }
+
+        return largestPrime;
+    }
+}
